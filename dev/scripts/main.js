d1f049eaf663e11b120327c489ee59bf76f408ea
Alla Borisova <alla.borisova27@gmail.com>
2024-02-09 19:16:39 +0500

    fix:bugs

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -101,4 +101,12 @@ jQuery(document).ready(function ($) {
       },
     ],
   });
+  $('.popup-youtube').magnificPopup({
+    disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: false,
+  });
 });
