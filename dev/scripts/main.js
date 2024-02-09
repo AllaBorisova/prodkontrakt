a5a2f7ad6b701dbc5fd8fe6a4a3b6ce5013a6dac
Alla Borisova <alla.borisova27@gmail.com>
2024-02-09 18:22:18 +0500

    fix:bugs

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -62,7 +62,10 @@ jQuery(document).ready(function ($) {
     speed: 300,
     slidesToShow: 1,
     centerMode: false,
-
+    prevArrow:
+      "<button type='button' class='slick-prev'><svg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'><g stroke='#F48C37'><circle cx='12' cy='12' r='10' transform='rotate(90 12 12)'/><path d='M13.5 15l-3-3 3-3' stroke-linecap='round' stroke-linejoin='round'/></g></svg></button>",
+    nextArrow:
+      "<button type='button' class='slick-next'><svg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'><g stroke='#F48C37'><circle cx='12' cy='12' r='10' transform='rotate(-90 12 12)'/><path d='M10.5 9l3 3-3 3' stroke-linecap='round' stroke-linejoin='round'/></g></svg></button>",
     responsive: [
       {
         breakpoint: 768,
@@ -71,10 +74,29 @@ jQuery(document).ready(function ($) {
           dots: false,
           centerMode: false,
           slidesToShow: 1,
-          // prevArrow:
-          //   '<button type="button" class="slick-prev-main"><svg><use xlink:href="./img/sprite.svg#arrow-left"></use></svg></button>',
-          // nextArrow:
-          //   '<button type="button" class="slick-next-main"><svg><use xlink:href="./img/sprite.svg#arrow-right"></use></svg></button>',
+        },
+      },
+    ],
+  });
+  $('.section__slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    centerMode: false,
+    prevArrow:
+      "<button type='button' class='slick-prev'><svg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'><g stroke='#F48C37'><circle cx='12' cy='12' r='10' transform='rotate(90 12 12)'/><path d='M13.5 15l-3-3 3-3' stroke-linecap='round' stroke-linejoin='round'/></g></svg></button>",
+    nextArrow:
+      "<button type='button' class='slick-next'><svg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'><g stroke='#F48C37'><circle cx='12' cy='12' r='10' transform='rotate(-90 12 12)'/><path d='M10.5 9l3 3-3 3' stroke-linecap='round' stroke-linejoin='round'/></g></svg></button>",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          arrows: false,
+          dots: false,
+          // centerMode: true,
+          variableWidth: true,
+          // slidesToShow: 3,
         },
       },
     ],
